64151a9f26b2063b780904fecb60abf917c50cc7
Brecht Ooms <brecht.ooms@hotmail.com>
2024-06-15 16:23:12 +0200

    Refactor bit conversion

---
+++ src/conversion.rs
@@ -0,0 +1,76 @@
+use std::array::from_fn;
+
+const SENSORS_PER_PANEL: usize = 12;
+
+type Panel = [u8; SENSORS_PER_PANEL];
+
+fn unpack_panel_int(panel: u16) -> Panel {
+    from_fn(|i| ((panel >> i) & 1) as u8)
+}
+
+#[inline(never)]
+fn unpack_board(chunk: [u8; 3]) -> [Panel; 2] {
+    let mut panels_int: [u16; 2] = [0, 0];
+
+    for (i, register) in chunk.into_iter().enumerate() {
+        let left_bits = register & 0x0F;
+        let right_bits = (register >> 4) & 0x0F;
+
+        panels_int[0] |= (left_bits as u16) << (i * 4);
+        panels_int[1] |= (right_bits as u16) << (i * 4);
+    }
+
+    panels_int.map(unpack_panel_int)
+}
+
+pub fn guarantee_size(chunk: &[u8]) -> [u8; 3] {
+    chunk.try_into().unwrap()
+}
+
+pub fn unpack_packet(packet: &[u8]) -> Vec<Panel> {
+    packet.chunks_exact(3).map(guarantee_size).flat_map(unpack_board).collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unpack_board() {
+        const BOARD: [u8; 3] = [
+            0b0000_1010,
+            0b1111_0100,
+            0b0011_1110,
+        ];
+
+        const EXPECTED: [Panel; 2] = [
+            [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+        ];
+
+        assert_eq!(unpack_board(BOARD), EXPECTED);
+    }
+
+    #[test]
+    fn test_unpack_packet() {
+        const PACKET: &[u8] = &[
+            0b0000_1010,
+            0b1111_0100,
+            0b0011_1110,
+            0b0110_1110,
+            0b1111_0110,
+            0b0111_1110,
+        ];
+
+        let expected: Vec<Panel> = vec![
+            [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+            [0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1],
+            [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+        ];
+
+        assert_eq!(unpack_packet(PACKET), expected);
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,6 @@
 use std::net::UdpSocket;
 
-const BITS_PER_REGISTER: usize = u8::BITS as usize;
-const REGISTERS_PER_BOARD: usize = 3;
-const SENSORS_PER_PANEL: usize = 12;
-const PANELS_PER_BOARD: usize = 2;
-
-type ShiftRegister = [u8; BITS_PER_REGISTER];
-type Panel = [u8; SENSORS_PER_PANEL];
-type ShiftRegisterBoard = [Panel; PANELS_PER_BOARD];
-
-const INPUT: &[u8] = &[
-    0, 1, 2,
-    3, 4, 5,
-    7, 8, 9,
-    4
-];
-
-fn unpack_u8(mut value: u8) -> ShiftRegister {
-    let mut result = ShiftRegister::default();
-    let mut idx = 0;
-
-    while value > 0 {
-        result[idx] = value & 1;
-        value >>= 1;
-        idx += 1;
-    }
-
-    result
-}
-
-fn unpack_board(chunk: &[u8]) -> Option<ShiftRegisterBoard> {
-    let x: Vec<u8> = chunk.iter().copied().flat_map(unpack_u8).collect();
-
-    Some([
-        x[..SENSORS_PER_PANEL].try_into().ok()?,
-        x[SENSORS_PER_PANEL..].try_into().ok()?,
-    ])
-}
-
-fn unpack_packet(packet: &[u8]) -> Vec<Panel> {
-    packet.chunks_exact(REGISTERS_PER_BOARD).filter_map(unpack_board).flatten().collect()
-}
+mod conversion;
 
 fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:7010")?;
@@ -51,7 +11,7 @@ fn main() -> std::io::Result<()> {
 
         eprintln!("Recv {packet_size} bytes");
 
-        let packet = unpack_packet(&buf[..packet_size]);
+        let packet = conversion::unpack_packet(&buf[..packet_size]);
         let json_bytes = serde_json::to_vec(&packet)?;
 
         socket.send_to(&json_bytes, "127.0.0.1:7000")?;
