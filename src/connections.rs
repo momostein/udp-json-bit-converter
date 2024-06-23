e6d6b411b68b33e01fffdf6cf4fb2a40bd410f83
Brecht Ooms <brecht.ooms@hotmail.com>
2024-06-23 16:24:35 +0200

    Refactor connections

---
+++ src/connections.rs
@@ -0,0 +1,73 @@
+use std::io;
+use std::io::{Error, ErrorKind};
+use std::net::SocketAddr::V4;
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, ToSocketAddrs, UdpSocket};
+use std::time::Duration;
+
+use crate::Args;
+
+const BEACON_DATA: &[u8] = &[0xDE, 0xAD, 0xBE, 0xEF];
+
+pub struct Connections {
+    pub socket: UdpSocket,
+    pub esp_addr: SocketAddr,
+    pub touch_designer_addr: SocketAddr,
+}
+
+impl Connections {
+    pub fn broadcast_beacon(&self) -> io::Result<usize> {
+        self.socket.send_to(BEACON_DATA, self.esp_addr)
+    }
+
+    pub fn recv_esp_data(&self, buf: &mut [u8]) -> io::Result<usize> {
+        loop {
+            match self.socket.recv(buf) {
+                Ok(data_len) => return Ok(data_len),
+                Err(err) => match err.kind() {
+                    ErrorKind::TimedOut => {
+                        eprintln!("Recv timeout, broadcasting beacon...");
+                        self.broadcast_beacon()?;
+                    }
+                    _ => {
+                        return Err(err);
+                    }
+                },
+            }
+        }
+    }
+
+    pub fn send_touch_designer_bytes(&self, bytes: &[u8]) -> io::Result<usize> {
+        self.socket.send_to(&bytes, self.touch_designer_addr)
+    }
+}
+
+pub fn open_connections(args: &Args) -> io::Result<Connections> {
+    let esp_addr = V4(SocketAddrV4::new(Ipv4Addr::BROADCAST, args.esp_port));
+    eprintln!("ESP address: {esp_addr}");
+
+    let touch_designer_addr =
+        args.touch_designer_addr
+            .to_socket_addrs()?
+            .next()
+            .ok_or(Error::new(
+                ErrorKind::InvalidInput,
+                "Invalid touch designer address",
+            ))?;
+    eprintln!("Touch Designer address: {}", touch_designer_addr);
+
+    let socket = UdpSocket::bind(SocketAddrV4::new(
+        Ipv4Addr::UNSPECIFIED,
+        args.converter_port.unwrap_or(0),
+    ))?;
+
+    socket.set_read_timeout(Some(Duration::from_millis(2000)))?;
+    socket.set_broadcast(true)?;
+
+    eprintln!("Socket bound to {}", socket.local_addr().unwrap());
+
+    Ok(Connections {
+        esp_addr,
+        touch_designer_addr,
+        socket,
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
-use std::net::UdpSocket;
+use clap::Parser;
+
+use bit_conversion::unpack_packet;
 
 mod bit_conversion;
+mod connections;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    #[arg(short, long, default_value_t = 7010)]
+    esp_port: u16,
+
+    #[arg(short, long)]
+    converter_port: Option<u16>,
+
+    #[arg(short, long, default_value = "127.0.0.1:7000")]
+    touch_designer_addr: String,
+}
 
 fn main() -> std::io::Result<()> {
-    let socket = UdpSocket::bind("0.0.0.0:7010")?;
+    let args = Args::parse();
+    let connections = connections::open_connections(&args)?;
 
+    connections.broadcast_beacon()?;
     loop {
         let mut buf = [0; 4096];
-        let packet_size = socket.recv(&mut buf)?;
+        let packet_size = connections.recv_esp_data(&mut buf)?;
 
-        eprintln!("Recv {packet_size} bytes");
+        let panels = unpack_packet(&buf[..packet_size]);
+        let json_bytes = serde_json::to_vec(&panels)?;
 
-        let packet = bit_conversion::unpack_packet(&buf[..packet_size]);
-        let json_bytes = serde_json::to_vec(&packet)?;
-
-        socket.send_to(&json_bytes, "127.0.0.1:7000")?;
+        connections.send_touch_designer_bytes(&json_bytes)?;
     }
 }
